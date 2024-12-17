7eec20778d976fd0f8c1703580d8cfe6eabd1fbd
Afiyotegy <afiyotegy06@gmail.com>
2024-12-17 13:05:40 +0530

    Project incomplete

--- app/src/main/java/com/example/movieapp/MainActivity.java
+++ app/src/main/java/com/example/movieapp/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.movieapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,12 +14,31 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+Button b;
+String s,ss,r="Invalid credentials";
+EditText et1,et2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-
+        et1=(EditText)findViewById(R.id.user);
+        et2=(EditText)findViewById(R.id.pass);
+        b=(Button) findViewById(R.id.log);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                s=et1.getText().toString();
+                ss=et2.getText().toString();
+                if(s.equals("Theatre") && ss.equals("123456"))
+                {
+                Intent ob=new Intent(getApplicationContext(), MenuActivity.class);
+                startActivity(ob);
+            }
+                else
+                {
+                    Toast.makeText(getApplicationContext(),r,Toast.LENGTH_LONG).show();
+                }}
+        });
         };
     }
---
+++ app/src/main/java/com/example/movieapp/MenuActivity.java
@@ -0,0 +1,42 @@
+package com.example.movieapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MenuActivity extends AppCompatActivity {
+Button b1,b2,b3,b4;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_menu);
+        b1=(Button) findViewById(R.id.add);
+        b2=(Button) findViewById(R.id.search);
+        b3=(Button) findViewById(R.id.view);
+        b4=(Button)findViewById(R.id.ex);
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent ob=new Intent(getApplicationContext(), AddActivity.class);
+                startActivity(ob);
+            }
+        });
+        b4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent ob=new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(ob);
+            }
+        });
+        }
+    }
